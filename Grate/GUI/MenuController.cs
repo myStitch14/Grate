e331b6107a2ca062617125aab3a8bb79b97c02eb
MonkyGT <monkygtstudios@gmail.com>
2025-04-16 20:59:40 -0400

    Comment Out Throw

--- Grate/GUI/MenuController.cs
+++ Grate/GUI/MenuController.cs
@@ -97,7 +97,7 @@ namespace Grate.GUI
                         gameObject.AddComponent<Boxing>(),
                         gameObject.AddComponent<Piggyback>(),
                         gameObject.AddComponent<Telekinesis>(),
-                        gameObject.AddComponent<Throw>(),
+                        //gameObject.AddComponent<Throw>(),
                         gameObject.AddComponent<Fireflies>(),
                         gameObject.AddComponent<ESP>(),
                         gameObject.AddComponent<RatSword>(),
